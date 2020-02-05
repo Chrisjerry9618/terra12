52d234ea89bb1fa170526a9bc2ace713ed1eb92d
root <root@DESKTOP-0QEAO1I.localdomain>
2020-02-05 16:15:40 +0530

    ebs-ec2

--- tv1.tf
+++ tv1.tf
@@ -12,21 +12,15 @@ resource "aws_instance" "ec2" {
   iam_instance_profile = "admin-access"
   associate_public_ip_address = "true"
   root_block_device{volume_size = 8}
-  ebs_block_device{volume_size = 8}
+  ebs_block_device {
+      device_name = "/dev/sdf"
+      volume_size = 8
+      volume_type = "gp2"
+  }
   vpc_security_group_ids = ["sg-03d34d9db9c65fcf5"]
    tags = {
     Name = "Devops-Terraform-BE-dev"
  }
-}
-resource "aws_volume_attachment" "ebs_att" {
-  device_name = "/dev/sdf"
-  volume_id   = "{aws_ebs_volume.secondry.id}"
-  instance_id = "${aws_instance.ec2.id}"
-}
-
-resource "aws_ebs_volume" "secondry" {
-  availability_zone = "us-east-1a"
-  size              = 8
 }
 
 
